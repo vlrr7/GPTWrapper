58da5079978fa312bce668f72f47efa9d48f7887
Valere <comptedecode@gmail.com>
2025-10-31 19:56:07 -0400

    modified main page to align w eslint

--- project/app/page.tsx
+++ project/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      "Hello World"
+      <p>&quot;Hello World&quot;</p>
     </div>
   );
 }
